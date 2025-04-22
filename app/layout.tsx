04ca8502a6e19853f27eed07d314457ebef9d0c2
Nick Carney <npcarney@uvm.edu>
2025-04-22 12:47:25 -0400

    vercel speed insights and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,8 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
